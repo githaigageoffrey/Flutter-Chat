2eeaed648bec512977414db07d884cc2035975e1
githaigageoffrey <githaiga.geoffrey@gmail.com>
2020-07-11 14:22:00 +0300

    First commit

---
+++ lib/screens/chat-screen.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection("chats/GcKNhSdcuTxtWowMHBDR/messages")
+            .snapshots(),
+        builder: (ctx, streamSnapShot) {
+          if(streamSnapShot.connectionState == ConnectionState.waiting){
+            return Center(child: CircularProgressIndicator(),);
+          }
+          final documents = streamSnapShot.data.documents;
+          print(documents);
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, index) => Container(
+                padding: EdgeInsets.all(10), child: Text(documents[index]["texts"])),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+          }),
+    );
+  }
+}
