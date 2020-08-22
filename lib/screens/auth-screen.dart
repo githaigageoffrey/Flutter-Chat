7581d29604b81fb38ea4bfd8a76f0841d79417fa
githaigageoffrey <githaiga.geoffrey@gmail.com>
2020-08-22 21:24:22 +0300

    auth screen

--- lib/screens/auth-screen.dart
+++ lib/screens/auth-screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_complete_guide/widgets/auth-form.dart';
+import 'package:flutter_complete_guide/widgets/auth/auth-form.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
