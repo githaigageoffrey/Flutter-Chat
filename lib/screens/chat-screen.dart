0441d2603888f0da2c7e56789168fd929ab3c2c6
githaigageoffrey <githaiga.geoffrey@gmail.com>
2020-08-22 22:15:15 +0300

    new message

--- lib/screens/chat-screen.dart
+++ lib/screens/chat-screen.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/widgets/chat/new-message.dart';
+import '../widgets/chat/messages.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -30,55 +32,44 @@ class _ChatScreenState extends State<ChatScreen>
         title: Text("My Chat App"),
         actions: [
           DropdownButton(
-            icon: Icon(
-              Icons.more_vert,
-              color: Theme.of(context).primaryIconTheme.color,
-            ),
-            items: [
-              DropdownMenuItem(child: Container(
-                child: Row(
-                  children: <Widget>[
-                    Icon( Icons.exit_to_app),
-                    SizedBox(width: 8,),
-                    Text("Logout"),
-                  ]
+              icon: Icon(
+                Icons.more_vert,
+                color: Theme.of(context).primaryIconTheme.color,
+              ),
+              items: [
+                DropdownMenuItem(
+                  child: Container(
+                    child: Row(children: <Widget>[
+                      Icon(Icons.exit_to_app),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text("Logout"),
+                    ]),
+                  ),
+                  value: 'logout',
                 ),
-              ),
-              value: 'logout',
-              ),
-            ],
-            onChanged: (itemidentifier){
-              if(itemidentifier=='logout'){
-                FirebaseAuth.instance.signOut();
-              }
-            }
-          ),
+              ],
+              onChanged: (itemidentifier) {
+                if (itemidentifier == 'logout') {
+                  FirebaseAuth.instance.signOut();
+                }
+              }),
         ],
       ),
-      body: StreamBuilder(
-        stream: Firestore.instance
-            .collection("chats/GcKNhSdcuTxtWowMHBDR/messages")
-            .snapshots(),
-        builder: (ctx, streamSnapShot) {
-          if(streamSnapShot.connectionState == ConnectionState.waiting){
-            return Center(child: CircularProgressIndicator(),);
-          }
-          final documents = streamSnapShot.data.documents;
-          return ListView.builder(
-            itemCount: documents.length,
-            itemBuilder: (ctx, index) => Container(
-                padding: EdgeInsets.all(10), child: Text(documents[index]["text"])),
-          );
-        },
+      body: Container(
+        child: Column(children: <Widget>[
+          Expanded(child: Messages()),
+          NewMessage(),
+        ]),
       ),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            Firestore.instance
-            .collection("chats/GcKNhSdcuTxtWowMHBDR/messages").add({
-              "text" : "This is a user added button chat"
-            });
-          }),
+      // floatingActionButton: FloatingActionButton(
+      //     child: Icon(Icons.add),
+      //     onPressed: () {
+      //       Firestore.instance
+      //           .collection("chats/GcKNhSdcuTxtWowMHBDR/messages")
+      //           .add({"text": "This is a user added button chat"});
+      //     }),
     );
   }
 }
